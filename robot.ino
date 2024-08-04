528412eeb665ce781dc706788907db4e7b8d75f3
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-08-04 16:18:22 +0700

    feat: Upload DC motor

--- robot.ino
+++ robot.ino
@@ -12,8 +12,7 @@
 
 // Arms: R1
 
-// Hard kick: L2
-// Soft kick: L1
+// Kick: L2 or R2
 
 // LIBRARY
 #include <Arduino.h>
@@ -39,16 +38,15 @@ const int armRightClose = 135;
 const int armRightOpen = 45;
 
 // DC motor
-const int directionPin = 7;
-const int pwmPin = 5;
-const int brakePin = 2;
+const int IN1 = 2;
+const int IN2 = 3;
 
 const int maxSpeed = 100;
 const int hardKickSpeed = 100;
 const int softKickSpeed = 60;
 
 // Sound
-const int soundPin = 3;
+const int soundPin = 4;
 
 // VARIABLES
 // States
@@ -76,94 +74,77 @@ bool directionState;
 
 void setup()
 {
-	// Engines
-	board.InitServomotorPort(PORT_D2);
-	board.InitServomotorPort(PORT_D4);
-	board.Servomotor(PORT_D2, 0);
-	board.Servomotor(PORT_D4, 0);
+    // Engines
+    board.InitServomotorPort(PORT_D2);
+    board.InitServomotorPort(PORT_D4);
+    board.Servomotor(PORT_D2, 0);
+    board.Servomotor(PORT_D4, 0);
 
-	// Arms
-	board.InitServomotorPort(PORT_D7);
-	board.InitServomotorPort(PORT_D8);
+    // Arms
+    board.InitServomotorPort(PORT_D7);
+    board.InitServomotorPort(PORT_D8);
 
-	// DC motor
-	pinMode(directionPin, OUTPUT);
-	pinMode(pwmPin, OUTPUT);
-	pinMode(brakePin, OUTPUT);
+    // DC motor
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
 
-	// Always release break
-	digitalWrite(brakePin, LOW);
+    // Sound
+    pinMode(soundPin, OUTPUT);
 
-	// Sound
-	pinMode(soundPin, OUTPUT);
+    // Serial
+    Serial.begin(9600);
 
-	// Serial
-	Serial.begin(9600);
-
-	// Controller
-	error = ps2x.config_gamepad(13, 11, 10, 12, true, true);
+    // Controller
+    error = ps2x.config_gamepad(13, 11, 10, 12, true, true);
 }
 
 // Turn on the robot
 void turnOn()
 {
-	// If green (triangle) button is pressed, the robot will move
-	if (ps2x.ButtonPressed(PSB_GREEN))
-	{
-		stop = 0;
-	}
+    // If green (triangle) button is pressed, the robot will move
+    if (ps2x.ButtonPressed(PSB_GREEN))
+    {
+        stop = 0;
+    }
 }
 
 // Turn off the robot
 void turnOff()
 {
-	// If blue (cross) button is pressed, the robot will stop
-	if (ps2x.ButtonPressed(PSB_BLUE))
-	{
-		stop = 1;
-	}
+    // If blue (cross) button is pressed, the robot will stop
+    if (ps2x.ButtonPressed(PSB_BLUE))
+    {
+        stop = 1;
+    }
 }
 
 void moveArms()
 {
-	// While pressing red button, the arm will move from 45 to 135 degrees
-	if (ps2x.Button(PSB_R1))
-	{
-		board.Servomotor(PORT_D7, armLeftClose);
-		board.Servomotor(PORT_D8, armRightClose);
-	}
-	else
-	{
-		board.Servomotor(PORT_D7, armLeftOpen);
-		board.Servomotor(PORT_D8, armRightOpen);
-	}
+    // While pressing red button, the arm will move from 45 to 135 degrees
+    if (ps2x.Button(PSB_R1))
+    {
+        board.Servomotor(PORT_D7, armLeftClose);
+        board.Servomotor(PORT_D8, armRightClose);
+    }
+    else
+    {
+        board.Servomotor(PORT_D7, armLeftOpen);
+        board.Servomotor(PORT_D8, armRightOpen);
+    }
 }
 
 void kick()
 {
-	// When pressing L1 button, the robot will make a soft kick: DC Motor will generate power in 1 second and return immediately
-	if (ps2x.Button(PSB_L1))
-	{
-		// DC motor will generate power in 1 second
-		digitalWrite(directionPin, HIGH);
-		analogWrite(pwmPin, softKickSpeed);
-
-		// DC motor will return immediately
-		digitalWrite(directionPin, LOW);
-		analogWrite(pwmPin, maxSpeed);
-	}
-
-	// When pressing L2 button, the robot will make a hard kick: DC Motor will generate power in 1 second and return immediately
-	if (ps2x.Button(PSB_L2))
-	{
-		// DC motor will generate power in 1 second
-		digitalWrite(directionPin, HIGH);
-		analogWrite(pwmPin, hardKickSpeed);
-
-		// DC motor will return immediately
-		digitalWrite(directionPin, LOW);
-		analogWrite(pwmPin, maxSpeed);
-	}
+    if (ps2x.Button(PSB_L2) || ps2x.Button(PSB_R2))
+    {
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+    }
+    else
+    {
+        digitalWrite(IN1, 0);
+        digitalWrite(IN2, 0);
+    }
 }
 
 void sound()
@@ -180,45 +161,45 @@ void sound()
 
 void moveRobot()
 {
-	forward = ps2x.Analog(PSS_LY) - 128;
-	turn = ps2x.Analog(PSS_RX) - 128;
+    forward = ps2x.Analog(PSS_LY) - 128;
+    turn = ps2x.Analog(PSS_RX) - 128;
 
-	// If the robot is stopped, the engines will be stopped
-	if (stop == 1)
-	{
-		angle = angle0;
-		angle2 = angle20;
-		board.Servomotor(PORT_D2, 0);
-		board.Servomotor(PORT_D4, 0);
-	}
-	// Otherwise, the robot will move
-	else
-	{
-		angle = angle0 - forward / 10 + turn / 10;
-		angle2 = angle20 + forward / 10 + turn / 10;
-		board.Servomotor(PORT_D2, angle);
-		board.Servomotor(PORT_D4, angle2);
-	}
+    // If the robot is stopped, the engines will be stopped
+    if (stop == 1)
+    {
+        angle = angle0;
+        angle2 = angle20;
+        board.Servomotor(PORT_D2, 0);
+        board.Servomotor(PORT_D4, 0);
+    }
+    // Otherwise, the robot will move
+    else
+    {
+        angle = angle0 - forward / 10 + turn / 10;
+        angle2 = angle20 + forward / 10 + turn / 10;
+        board.Servomotor(PORT_D2, angle);
+        board.Servomotor(PORT_D4, angle2);
+    }
 }
 void loop()
 {
-	ps2x.read_gamepad(false, vibrate); // read controller
+    ps2x.read_gamepad(false, vibrate); // read controller
 
-	// Change the state of robot
-	turnOn();
-	turnOff();
+    // Change the state of robot
+    turnOn();
+    turnOff();
 
-	// Play sound
-	sound();
+    // Play sound
+    sound();
 
-	// Move arms
-	moveArms();
+    // Move arms
+    moveArms();
 
-	// Move robot
-	moveRobot();
+    // Move robot
+    moveRobot();
 
-	// Kick
-	kick();
+    // Kick
+    kick();
 
-	delay(generalDelay);
+    delay(generalDelay);
 }
